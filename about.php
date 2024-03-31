e1f300f232eb4391dcc4bcc9042cd605bf8dd21d
ShresthaShulav <162817352+ShresthaShulav@users.noreply.github.com>
2024-03-31 21:30:55 +0545

    footer fix

--- about.php
+++ about.php
@@ -1,16 +1,16 @@
-<?php require_once('header.php'); ?>
+<?php require_once ('header.php'); ?>
 
 <?php
 $statement = $pdo->prepare("SELECT * FROM tbl_page WHERE id=1");
 $statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach ($result as $row) {
-   $about_title = $row['about_title'];
+    $about_title = $row['about_title'];
     $about_content = $row['about_content'];
     $about_banner = $row['about_banner'];
 }
 ?>
-
+<!-- 
 <div class="page-banner" style="background-image: url(assets/uploads/<?php echo $about_banner; ?>);">
     <div class="inner">
         <h1><?php echo $about_title; ?></h1>
@@ -27,6 +27,45 @@ foreach ($result as $row) {
             </div>
         </div>
     </div>
-</div>
+</div> -->
+<section
+    class="tw-w-[1370px] tw-flex tw-flex-row tw-items-start tw-justify-end tw-pt-0 tw-px-[46px] tw-pb-7 tw-box-border tw-max-w-full tw-text-left tw-text-29xl tw-text-light-gray-11 tw-font-lato lg:tw-pl-[23px] lg:tw-pr-[23px] lg:tw-box-border">
+    <div class="tw-flex-1 tw-flex tw-flex-col tw-items-start tw-justify-start tw-gap-[6px] tw-max-w-full">
+        <div
+            class="tw-w-[590px] tw-flex tw-flex-row tw-items-start tw-justify-start tw-py-0 tw-px-[5px] tw-box-border tw-max-w-full">
+            <h2
+                class="tw-m-0 tw-flex-1 tw-relative tw-text-inherit tw-font-black tw-font-inherit tw-inline-block tw-max-w-full tw-mq450:text-10xl tw-mq1050:text-19xl">
+                About Us
+            </h2>
+        </div>
+        <div
+            class="tw-self-stretch tw-flex tw-flex-row tw-items-start tw-justify-start tw-gap-[69px] tw-max-w-full tw-text-5xl lg:tw-flex-wrap tw-mq450:gap-[69px_17px] tw-mq750:gap-[69px_34px]">
+            <div
+                class="tw-w-[580px] tw-flex tw-flex-col tw-items-start tw-justify-start tw-pt-[21px] tw-px-0 tw-pb-0 tw-box-border tw-min-w-[580px] tw-max-w-full lg:tw-flex-1 tw-mq750:min-w-full">
+                <div class="tw-self-stretch tw-relative tw-mq450:text-lgi">
+                    <p class="tw-m-0">
+                        Started as a small plant nursery plant planet is now offering
+                        home delivery service of various plant varities in 35+ cities.
+                    </p>
+                    <p class="tw-m-0">&nbsp;</p>
+                    <p class="tw-m-0">
+                        We work in the motto clean air with healthy plants. All the
+                        plants are grown with natural manure.
+                    </p>
+                    <p class="tw-m-0">&nbsp;</p>
+                    <p class="tw-m-0">Hope to hear more from you</p>
+                    <p class="tw-m-0">&nbsp;</p>
+                    <p class="tw-m-0">Team</p>
+                    <p class="tw-m-0">Plant Plant</p>
+                </div>
+            </div>
+            <div
+                class="tw-h-[373px] tw-flex-1 tw-relative tw-rounded-xl tw-min-w-[409px] tw-flex tw-items-center tw-justify-center">
+                <img class="tw-h-full tw-flex-1 tw-max-w-full tw-overflow-hidden tw-min-w-[409px] tw-object-contain tw-absolute tw-left-[0px] tw-top-[7px] tw-w-full [transform:scale(1.038)] tw-mq750:min-w-full"
+                    loading="lazy" alt="" src="public/rectangle-17@2x.png" />
+            </div>
+        </div>
+    </div>
+</section>
 
-<?php require_once('footer.php'); ?>
+<?php require_once ('footer.php'); ?>
--- footer.php
+++ footer.php
@@ -16,115 +16,6 @@ foreach ($result as $row) {
 ?>
 
 
-<?php if ($newsletter_on_off == 1): ?>
-  <section class="home-newsletter">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-          <div class="single">
-            <?php
-            if (isset($_POST['form_subscribe'])) {
-
-              if (empty($_POST['email_subscribe'])) {
-                $valid = 0;
-                $error_message1 .= LANG_VALUE_131;
-              } else {
-                if (filter_var($_POST['email_subscribe'], FILTER_VALIDATE_EMAIL) === false) {
-                  $valid = 0;
-                  $error_message1 .= LANG_VALUE_134;
-                } else {
-                  $statement = $pdo->prepare("SELECT * FROM tbl_subscriber WHERE subs_email=?");
-                  $statement->execute(array($_POST['email_subscribe']));
-                  $total = $statement->rowCount();
-                  if ($total) {
-                    $valid = 0;
-                    $error_message1 .= LANG_VALUE_147;
-                  } else {
-                    // Sending email to the requested subscriber for email confirmation
-                    // Getting activation key to send via email. also it will be saved to database until user click on the activation link.
-                    $key = md5(uniqid(rand(), true));
-
-                    // Getting current date
-                    $current_date = date('Y-m-d');
-
-                    // Getting current date and time
-                    $current_date_time = date('Y-m-d H:i:s');
-
-                    // Inserting data into the database
-                    $statement = $pdo->prepare("INSERT INTO tbl_subscriber (subs_email,subs_date,subs_date_time,subs_hash,subs_active) VALUES (?,?,?,?,?)");
-                    $statement->execute(array($_POST['email_subscribe'], $current_date, $current_date_time, $key, 0));
-
-                    // Sending Confirmation Email
-                    $to = $_POST['email_subscribe'];
-                    $subject = 'Subscriber Email Confirmation';
-
-                    // Getting the url of the verification link
-                    $verification_url = BASE_URL . 'verify.php?email=' . $to . '&key=' . $key;
-
-                    $message = '
-Thanks for your interest to subscribe our newsletter!<br><br>
-Please click this link to confirm your subscription:
-					' . $verification_url . '<br><br>
-This link will be active only for 24 hours.
-					';
-
-                    $headers = 'From: ' . $contact_email . "\r\n" .
-                      'Reply-To: ' . $contact_email . "\r\n" .
-                      'X-Mailer: PHP/' . phpversion() . "\r\n" .
-                      "MIME-Version: 1.0\r\n" .
-                      "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-                    // Sending the email
-                    mail($to, $subject, $message, $headers);
-
-                    $success_message1 = LANG_VALUE_136;
-                  }
-                }
-              }
-            }
-            if ($error_message1 != '') {
-              echo "<script>alert('" . $error_message1 . "')</script>";
-            }
-            if ($success_message1 != '') {
-              echo "<script>alert('" . $success_message1 . "')</script>";
-            }
-            ?>
-            <form action="" method="post">
-              <?php $csrf->echoInputField(); ?>
-              <h2>
-                <?php echo LANG_VALUE_93; ?>
-              </h2>
-              <div class="input-group">
-                <input type="email" class="form-control" placeholder="<?php echo LANG_VALUE_95; ?>"
-                  name="email_subscribe">
-                <span class="input-group-btn">
-                  <button class="btn btn-theme" type="submit" name="form_subscribe">
-                    <?php echo LANG_VALUE_92; ?>
-                  </button>
-                </span>
-              </div>
-          </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
-<?php endif; ?>
-
-
-
-
-<!-- <div class="footer-bottom">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 copyright">
-        <?php echo $footer_copyright; ?>
-      </div>
-    </div>
-  </div>
-</div> -->
-
-
 <footer
   class="tw-self-stretch tw-bg-darkslategray-400 tw-overflow-hidden tw-flex tw-flex-col tw-items-center tw-justify-end tw-py-16 tw-px-20 tw-box-border tw-max-w-full tw-text-left tw-text-3xs tw-text-gainsboro-300 tw-font-body-2-medium tw-mq750:py-[42px] tw-mq750:px-10 tw-mq750:box-border">
   <div
@@ -170,10 +61,10 @@ This link will be active only for 24 hours.
           </div>
           <div
             class="tw-flex-[0.5551] tw-flex tw-flex-col tw-items-start tw-justify-start tw-pt-[46px] tw-pb-0 tw-pr-[182px] tw-pl-0 tw-box-border tw-min-w-[200px] tw-text-9xl tw-font-spectral tw-mq450:pr-5 tw-mq450:box-border tw-mq450:flex-1">
-            <div
+            <a href="about.php"
               class="tw-relative tw-tracking-[-0.02em] tw-leading-[34px] tw-font-extrabold tw-inline-block tw-min-w-[116px] tw-mq450:text-3xl tw-mq450:leading-[27px]">
               About us
-            </div>
+            </a>
             <div
               class="tw-relative tw-tracking-[-0.02em] tw-leading-[34px] tw-font-extrabold tw-inline-block tw-min-w-[126px] tw-mq450:text-3xl tw-mq450:leading-[27px]">
               View Cart
