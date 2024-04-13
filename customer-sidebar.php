41b01159d8f4cc5041ee0ca36b0a6c707440d0c6
ShresthaShulav <162817352+ShresthaShulav@users.noreply.github.com>
2024-04-13 23:34:46 +0545

    updated cart, product and dashboard

--- customer-sidebar.php
+++ customer-sidebar.php
@@ -1,10 +1,82 @@
-<div class="user-sidebar">
-    <ul>
-    <a href="dashboard.php"><button class="btn btn-danger"><?php echo LANG_VALUE_89; ?></button></a>
-    <a href="customer-profile-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_117; ?></button></a>
-        <a href="customer-billing-shipping-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_88; ?></button></a>
-        <a href="customer-password-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_99; ?></button></a>
-        <a href="customer-order.php"><button class="btn btn-danger"><?php echo LANG_VALUE_24; ?></button></a>
-        <a href="logout.php"><button class="btn btn-danger"><?php echo LANG_VALUE_14; ?></button></a>
-    </ul>
-</div>
+<section>
+    <div class="tw-container tw-px-5 tw-mx-auto">
+        <div class="tw-flex tw-flex-wrap tw-m-4 tw-text-center">
+            <a href="customer-profile-update.php" class="tw-p-4 tw-md:w-1/4 tw-sm:w-1/2 tw-w-1/5">
+                <div
+                    class="tw-border-2 tw-border-black tw-px-4 tw-py-6 tw-rounded-lg tw-transform tw-transition tw-duration-500 tw-hover:scale-110">
+                    <svg fill="#4B0082" viewBox="0 -64 640 640"
+                        class="tw-text-indigo-500 tw-w-12 tw-h-12 tw-mb-3 tw-inline-block">
+                        <path
+                            d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h274.9c-2.4-6.8-3.4-14-2.6-21.3l6.8-60.9 1.2-11.1 7.9-7.9 77.3-77.3c-24.5-27.7-60-45.5-99.9-45.5zm45.3 145.3l-6.8 61c-1.1 10.2 7.5 18.8 17.6 17.6l60.9-6.8 137.9-137.9-71.7-71.7-137.9 137.8zM633 268.9L595.1 231c-9.3-9.3-24.5-9.3-33.8 0l-37.8 37.8-4.1 4.1 71.8 71.7 41.8-41.8c9.3-9.4 9.3-24.5 0-33.9z" />
+                    </svg>
+                    <h2 class="tw-title-font tw-font-medium tw-text-3xl tw-text-gray-900"><?php echo LANG_VALUE_117; ?>
+                    </h2>
+                </div>
+            </a>
+            <a href="customer-billing-shipping-update.php" class="tw-p-4 tw-md:w-1/4 tw-sm:w-1/2 tw-w-1/5">
+                <div
+                    class="tw-border-2 tw-border-gray-600 tw-px-4 tw-py-6 tw-rounded-lg tw-transform tw-transition tw-duration-500 tw-hover:scale-110">
+                    <svg viewBox="0 0 24 24" class="tw-text-indigo-500 tw-w-12 tw-h-12 tw-mb-3 tw-inline-block"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+                            stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <h2 class="tw-title-font tw-font-medium tw-text-3xl tw-text-gray-900">Update Bill Info
+                    </h2>
+                </div>
+            </a>
+            <a href="customer-password-update.php" class="tw-p-4 tw-md:w-1/4 tw-sm:w-1/2 tw-w-1/5">
+                <div
+                    class="tw-border-2 tw-border-gray-600 tw-px-4 tw-py-6 tw-rounded-lg tw-transform tw-transition tw-duration-500 tw-hover:scale-110">
+                    <svg viewBox="0 0 24 24" class="tw-text-indigo-500 tw-w-12 tw-h-12 tw-mb-3 tw-inline-block">
+                        <path
+                            d="M22 12C22 15.7712 22 17.6569 20.8284 18.8284C19.6569 20 17.7712 20 14 20H10C6.22876 20 4.34315 20 3.17157 18.8284C2 17.6569 2 15.7712 2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C21.4816 5.82475 21.7706 6.69989 21.8985 8"
+                            stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" />
+                        <path
+                            d="M9 12C9 12.5523 8.55228 13 8 13C7.44772 13 7 12.5523 7 12C7 11.4477 7.44772 11 8 11C8.55228 11 9 11.4477 9 12Z"
+                            fill="#1C274C" />
+                        <path
+                            d="M13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12Z"
+                            fill="#1C274C" />
+                        <path
+                            d="M17 12C17 12.5523 16.5523 13 16 13C15.4477 13 15 12.5523 15 12C15 11.4477 15.4477 11 16 11C16.5523 11 17 11.4477 17 12Z"
+                            fill="#1C274C" />
+                    </svg>
+                    <h2 class="tw-title-font tw-font-medium tw-text-3xl tw-text-gray-900"><?php echo LANG_VALUE_99; ?>
+                    </h2>
+                </div>
+            </a>
+            <a href="customer-order.php" class="tw-p-4 tw-md:w-1/4 tw-sm:w-1/2 tw-w-1/5">
+                <div
+                    class="tw-border-2 tw-border-gray-600 tw-px-4 tw-py-6 tw-rounded-lg tw-transform tw-transition tw-duration-500 tw-hover:scale-110">
+
+                    <svg viewBox="0 0 1024 1024" class="tw-text-indigo-500 tw-w-12 tw-h-12 tw-mb-3 tw-inline-block">
+                        <path
+                            d="M53.6 1023.2c-6.4 0-12.8-2.4-17.6-8-4.8-4.8-7.2-11.2-6.4-18.4L80 222.4c0.8-12.8 11.2-22.4 24-22.4h211.2v-3.2c0-52.8 20.8-101.6 57.6-139.2C410.4 21.6 459.2 0.8 512 0.8c108 0 196.8 88 196.8 196.8 0 0.8-0.8 1.6-0.8 2.4v0.8H920c12.8 0 23.2 9.6 24 22.4l49.6 768.8c0.8 2.4 0.8 4 0.8 6.4-0.8 13.6-11.2 24.8-24.8 24.8H53.6z m25.6-48H944l-46.4-726.4H708v57.6h0.8c12.8 8.8 20 21.6 20 36 0 24.8-20 44.8-44.8 44.8s-44.8-20-44.8-44.8c0-14.4 7.2-27.2 20-36h0.8v-57.6H363.2v57.6h0.8c12.8 8.8 20 21.6 20 36 0 24.8-20 44.8-44.8 44.8-24.8 0-44.8-20-44.8-44.8 0-14.4 7.2-27.2 20-36h0.8v-57.6H125.6l-46.4 726.4zM512 49.6c-81.6 0-148.8 66.4-148.8 148.8v3.2h298.4l-0.8-1.6v-1.6c0-82.4-67.2-148.8-148.8-148.8z"
+                            fill="" />
+                    </svg>
+                    <h2 class="tw-title-font tw-font-medium tw-text-3xl tw-text-gray-900"><?php echo LANG_VALUE_24; ?>
+                    </h2>
+                </div>
+            </a>
+            <a href="logout.php" class="tw-p-4 tw-md:w-1/4 tw-sm:w-1/2 tw-w-1/5">
+                <div
+                    class="tw-border-2 tw-border-gray-600 tw-px-4 tw-py-6 tw-rounded-lg tw-transform tw-transition tw-duration-500 tw-hover:scale-110">
+
+                    <svg viewBox="0 0 24 24" class="tw-text-indigo-500 tw-w-12 tw-h-12 tw-mb-3 tw-inline-block"
+                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15" stroke="#1C274C" stroke-width="1.5"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                        <path
+                            d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.3531 21.8897 19.1752 21.9862 17 21.9983M9.00195 17C9.01406 19.175 9.11051 20.3529 9.87889 21.1213C10.5202 21.7626 11.4467 21.9359 13 21.9827"
+                            stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" />
+                    </svg>
+                    <h2 class="tw-title-font tw-font-medium tw-text-3xl tw-text-gray-900"><?php echo LANG_VALUE_14; ?>
+                    </h2>
+                </div>
+            </a>
+
+        </div>
+    </div>
+</section>
