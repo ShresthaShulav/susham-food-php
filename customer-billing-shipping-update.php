9142d7be95dec5fa112bd6ece78119caea14a541
ShresthaShulav <162817352+ShresthaShulav@users.noreply.github.com>
2024-04-13 23:34:46 +0545

    updated cart, product and dashboard

--- customer-billing-shipping-update.php
+++ customer-billing-shipping-update.php
@@ -1,17 +1,17 @@
-<?php require_once('header.php'); ?>
+<?php require_once ('header.php'); ?>
 
 <?php
 // Check if the customer is logged in or not
-if(!isset($_SESSION['customer'])) {
-    header('location: '.BASE_URL.'logout.php');
+if (!isset($_SESSION['customer'])) {
+    header('location: ' . BASE_URL . 'logout.php');
     exit;
 } else {
     // If customer is logged in, but admin make him inactive, then force logout this user.
     $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id=? AND cust_status=?");
-    $statement->execute(array($_SESSION['customer']['cust_id'],0));
+    $statement->execute(array($_SESSION['customer']['cust_id'], 0));
     $total = $statement->rowCount();
-    if($total) {
-        header('location: '.BASE_URL.'logout.php');
+    if ($total) {
+        header('location: ' . BASE_URL . 'logout.php');
         exit;
     }
 }
@@ -41,26 +41,28 @@ if (isset($_POST['form1'])) {
                             cust_s_zip=? 
 
                             WHERE cust_id=?");
-    $statement->execute(array(
-                            strip_tags($_POST['cust_b_name']),
-                            strip_tags($_POST['cust_b_cname']),
-                            strip_tags($_POST['cust_b_phone']),
-                            strip_tags($_POST['cust_b_country']),
-                            strip_tags($_POST['cust_b_address']),
-                            strip_tags($_POST['cust_b_city']),
-                            strip_tags($_POST['cust_b_state']),
-                            strip_tags($_POST['cust_b_zip']),
-                            strip_tags($_POST['cust_s_name']),
-                            strip_tags($_POST['cust_s_cname']),
-                            strip_tags($_POST['cust_s_phone']),
-                            strip_tags($_POST['cust_s_country']),
-                            strip_tags($_POST['cust_s_address']),
-                            strip_tags($_POST['cust_s_city']),
-                            strip_tags($_POST['cust_s_state']),
-                            strip_tags($_POST['cust_s_zip']),
-                            $_SESSION['customer']['cust_id']
-                        ));  
-   
+    $statement->execute(
+        array(
+            strip_tags($_POST['cust_b_name']),
+            strip_tags($_POST['cust_b_cname']),
+            strip_tags($_POST['cust_b_phone']),
+            strip_tags($_POST['cust_b_country']),
+            strip_tags($_POST['cust_b_address']),
+            strip_tags($_POST['cust_b_city']),
+            strip_tags($_POST['cust_b_state']),
+            strip_tags($_POST['cust_b_zip']),
+            strip_tags($_POST['cust_s_name']),
+            strip_tags($_POST['cust_s_cname']),
+            strip_tags($_POST['cust_s_phone']),
+            strip_tags($_POST['cust_s_country']),
+            strip_tags($_POST['cust_s_address']),
+            strip_tags($_POST['cust_s_city']),
+            strip_tags($_POST['cust_s_state']),
+            strip_tags($_POST['cust_s_zip']),
+            $_SESSION['customer']['cust_id']
+        )
+    );
+
     $success_message = LANG_VALUE_122;
 
     $_SESSION['customer']['cust_b_name'] = strip_tags($_POST['cust_b_name']);
@@ -85,18 +87,18 @@ if (isset($_POST['form1'])) {
 
 <div class="page">
     <div class="container">
-        <div class="row">            
-            <div class="col-md-12"> 
-                <?php require_once('customer-sidebar.php'); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php require_once ('customer-sidebar.php'); ?>
             </div>
             <div class="col-md-12">
                 <div class="user-content">
                     <?php
-                    if($error_message != '') {
-                        echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
+                    if ($error_message != '') {
+                        echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>" . $error_message . "</div>";
                     }
-                    if($success_message != '') {
-                        echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
+                    if ($success_message != '') {
+                        echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>" . $success_message . "</div>";
                     }
                     ?>
                     <form action="" method="post">
@@ -106,15 +108,18 @@ if (isset($_POST['form1'])) {
                                 <h3><?php echo LANG_VALUE_86; ?></h3>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_102; ?></label>
-                                    <input type="text" class="form-control" name="cust_b_name" value="<?php echo $_SESSION['customer']['cust_b_name']; ?>">
+                                    <input type="text" class="form-control" name="cust_b_name"
+                                        value="<?php echo $_SESSION['customer']['cust_b_name']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_103; ?></label>
-                                    <input type="text" class="form-control" name="cust_b_cname" value="<?php echo $_SESSION['customer']['cust_b_cname']; ?>">
+                                    <input type="text" class="form-control" name="cust_b_cname"
+                                        value="<?php echo $_SESSION['customer']['cust_b_cname']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_104; ?></label>
-                                    <input type="text" class="form-control" name="cust_b_phone" value="<?php echo $_SESSION['customer']['cust_b_phone']; ?>">
+                                    <input type="text" class="form-control" name="cust_b_phone"
+                                        value="<?php echo $_SESSION['customer']['cust_b_phone']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_106; ?></label>
@@ -125,7 +130,9 @@ if (isset($_POST['form1'])) {
                                         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
                                         foreach ($result as $row) {
                                             ?>
-                                            <option value="<?php echo $row['country_id']; ?>" <?php if($row['country_id'] == $_SESSION['customer']['cust_b_country']) {echo 'selected';} ?>><?php echo $row['country_name']; ?></option>
+                                            <option value="<?php echo $row['country_id']; ?>" <?php if ($row['country_id'] == $_SESSION['customer']['cust_b_country']) {
+                                                   echo 'selected';
+                                               } ?>><?php echo $row['country_name']; ?></option>
                                             <?php
                                         }
                                         ?>
@@ -133,34 +140,41 @@ if (isset($_POST['form1'])) {
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_105; ?></label>
-                                    <textarea name="cust_b_address" class="form-control" cols="30" rows="10" style="height:100px;"><?php echo $_SESSION['customer']['cust_b_address']; ?></textarea>
+                                    <textarea name="cust_b_address" class="form-control" cols="30" rows="10"
+                                        style="height:100px;"><?php echo $_SESSION['customer']['cust_b_address']; ?></textarea>
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_107; ?></label>
-                                    <input type="text" class="form-control" name="cust_b_city" value="<?php echo $_SESSION['customer']['cust_b_city']; ?>">
+                                    <input type="text" class="form-control" name="cust_b_city"
+                                        value="<?php echo $_SESSION['customer']['cust_b_city']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_108; ?></label>
-                                    <input type="text" class="form-control" name="cust_b_state" value="<?php echo $_SESSION['customer']['cust_b_state']; ?>">
+                                    <input type="text" class="form-control" name="cust_b_state"
+                                        value="<?php echo $_SESSION['customer']['cust_b_state']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_109; ?></label>
-                                    <input type="text" class="form-control" name="cust_b_zip" value="<?php echo $_SESSION['customer']['cust_b_zip']; ?>">
+                                    <input type="text" class="form-control" name="cust_b_zip"
+                                        value="<?php echo $_SESSION['customer']['cust_b_zip']; ?>">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <h3><?php echo LANG_VALUE_87; ?></h3>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_102; ?></label>
-                                    <input type="text" class="form-control" name="cust_s_name" value="<?php echo $_SESSION['customer']['cust_s_name']; ?>">
+                                    <input type="text" class="form-control" name="cust_s_name"
+                                        value="<?php echo $_SESSION['customer']['cust_s_name']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_103; ?></label>
-                                    <input type="text" class="form-control" name="cust_s_cname" value="<?php echo $_SESSION['customer']['cust_s_cname']; ?>">
+                                    <input type="text" class="form-control" name="cust_s_cname"
+                                        value="<?php echo $_SESSION['customer']['cust_s_cname']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_104; ?></label>
-                                    <input type="text" class="form-control" name="cust_s_phone" value="<?php echo $_SESSION['customer']['cust_s_phone']; ?>">
+                                    <input type="text" class="form-control" name="cust_s_phone"
+                                        value="<?php echo $_SESSION['customer']['cust_s_phone']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_106; ?></label>
@@ -171,7 +185,9 @@ if (isset($_POST['form1'])) {
                                         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
                                         foreach ($result as $row) {
                                             ?>
-                                            <option value="<?php echo $row['country_id']; ?>" <?php if($row['country_id'] == $_SESSION['customer']['cust_s_country']) {echo 'selected';} ?>><?php echo $row['country_name']; ?></option>
+                                            <option value="<?php echo $row['country_id']; ?>" <?php if ($row['country_id'] == $_SESSION['customer']['cust_s_country']) {
+                                                   echo 'selected';
+                                               } ?>><?php echo $row['country_name']; ?></option>
                                             <?php
                                         }
                                         ?>
@@ -179,29 +195,36 @@ if (isset($_POST['form1'])) {
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_105; ?></label>
-                                    <textarea name="cust_s_address" class="form-control" cols="30" rows="10" style="height:100px;"><?php echo $_SESSION['customer']['cust_s_address']; ?></textarea>
+                                    <textarea name="cust_s_address" class="form-control" cols="30" rows="10"
+                                        style="height:100px;"><?php echo $_SESSION['customer']['cust_s_address']; ?></textarea>
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_107; ?></label>
-                                    <input type="text" class="form-control" name="cust_s_city" value="<?php echo $_SESSION['customer']['cust_s_city']; ?>">
+                                    <input type="text" class="form-control" name="cust_s_city"
+                                        value="<?php echo $_SESSION['customer']['cust_s_city']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_108; ?></label>
-                                    <input type="text" class="form-control" name="cust_s_state" value="<?php echo $_SESSION['customer']['cust_s_state']; ?>">
+                                    <input type="text" class="form-control" name="cust_s_state"
+                                        value="<?php echo $_SESSION['customer']['cust_s_state']; ?>">
                                 </div>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_109; ?></label>
-                                    <input type="text" class="form-control" name="cust_s_zip" value="<?php echo $_SESSION['customer']['cust_s_zip']; ?>">
+                                    <input type="text" class="form-control" name="cust_s_zip"
+                                        value="<?php echo $_SESSION['customer']['cust_s_zip']; ?>">
                                 </div>
                             </div>
                         </div>
-                        <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_5; ?>" name="form1">
+                        <input type="submit"
+                            class="tw-bg-blue-500 tw-hover:bg-blue-700 tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded"
+                            value="<?php echo LANG_VALUE_5; ?>" name="form1">
+                        </input>
                     </form>
-                </div>                
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 
-<?php require_once('footer.php'); ?>
+<?php require_once ('footer.php'); ?>
--- customer-password-update.php
+++ customer-password-update.php
@@ -1,17 +1,17 @@
-<?php require_once('header.php'); ?>
+<?php require_once ('header.php'); ?>
 
 <?php
 // Check if the customer is logged in or not
-if(!isset($_SESSION['customer'])) {
-    header('location: '.BASE_URL.'logout.php');
+if (!isset($_SESSION['customer'])) {
+    header('location: ' . BASE_URL . 'logout.php');
     exit;
 } else {
     // If customer is logged in, but admin make him inactive, then force logout this user.
     $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id=? AND cust_status=?");
-    $statement->execute(array($_SESSION['customer']['cust_id'],0));
+    $statement->execute(array($_SESSION['customer']['cust_id'], 0));
     $total = $statement->rowCount();
-    if($total) {
-        header('location: '.BASE_URL.'logout.php');
+    if ($total) {
+        header('location: ' . BASE_URL . 'logout.php');
         exit;
     }
 }
@@ -22,28 +22,28 @@ if (isset($_POST['form1'])) {
 
     $valid = 1;
 
-    if( empty($_POST['cust_password']) || empty($_POST['cust_re_password']) ) {
+    if (empty($_POST['cust_password']) || empty($_POST['cust_re_password'])) {
         $valid = 0;
-        $error_message .= LANG_VALUE_138."<br>";
+        $error_message .= LANG_VALUE_138 . "<br>";
     }
 
-    if( !empty($_POST['cust_password']) && !empty($_POST['cust_re_password']) ) {
-        if($_POST['cust_password'] != $_POST['cust_re_password']) {
+    if (!empty($_POST['cust_password']) && !empty($_POST['cust_re_password'])) {
+        if ($_POST['cust_password'] != $_POST['cust_re_password']) {
             $valid = 0;
-            $error_message .= LANG_VALUE_139."<br>";
+            $error_message .= LANG_VALUE_139 . "<br>";
         }
     }
-    
-    if($valid == 1) {
+
+    if ($valid == 1) {
 
         // update data into the database
 
         $password = strip_tags($_POST['cust_password']);
-        
+
         $statement = $pdo->prepare("UPDATE tbl_customer SET cust_password=? WHERE cust_id=?");
-        $statement->execute(array(md5($password),$_SESSION['customer']['cust_id']));
-        
-        $_SESSION['customer']['cust_password'] = md5($password);        
+        $statement->execute(array(md5($password), $_SESSION['customer']['cust_id']));
+
+        $_SESSION['customer']['cust_password'] = md5($password);
 
         $success_message = LANG_VALUE_141;
     }
@@ -52,9 +52,9 @@ if (isset($_POST['form1'])) {
 
 <div class="page">
     <div class="container">
-        <div class="row">            
-            <div class="col-md-12"> 
-                <?php require_once('customer-sidebar.php'); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php require_once ('customer-sidebar.php'); ?>
             </div>
             <div class="col-md-12">
                 <div class="user-content">
@@ -67,11 +67,11 @@ if (isset($_POST['form1'])) {
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
                                 <?php
-                                if($error_message != '') {
-                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
+                                if ($error_message != '') {
+                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>" . $error_message . "</div>";
                                 }
-                                if($success_message != '') {
-                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
+                                if ($success_message != '') {
+                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>" . $success_message . "</div>";
                                 }
                                 ?>
                                 <div class="form-group">
@@ -82,16 +82,19 @@ if (isset($_POST['form1'])) {
                                     <label for=""><?php echo LANG_VALUE_101; ?> *</label>
                                     <input type="password" class="form-control" name="cust_re_password">
                                 </div>
-                                <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_5; ?>" name="form1">
+                                <input type="submit"
+                                    class="tw-bg-blue-500 tw-hover:bg-blue-700 tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded"
+                                    value="<?php echo LANG_VALUE_5; ?>" name="form1">
+                                </input>
                             </div>
                         </div>
-                        
+
                     </form>
-                </div>                
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 
-<?php require_once('footer.php'); ?>
+<?php require_once ('footer.php'); ?>
